8175f1169583c0103f7d3c2b1f579d47ed0a6ac6
Yintong <zhouyintong96@gmail.com>
2019-11-25 16:47:24 +0100

    style update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ seedDB();
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }); // mongoDB connection
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + "/public"));
 app.get("", function (req, res) {
     res.render("landing");
 });
